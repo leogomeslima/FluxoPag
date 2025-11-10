e4b4a20ecf9f76021e67225a9aea90e7011cdfe8
leogomeslima <leonardo.glima2017@gmail.com>
2025-11-10 06:40:30 -0300

    feat(application): implementar handler para criação e processamento de pagamento
    - Adicionado o handler `CriarComandoDePagamentoHandler`, responsável por orquestrar o fluxo de criação e processamento de pagamentos.
    - O handler utiliza `IGatewayPagamento` para integração com o serviço externo de pagamento.
    - Implementada a persistência do pagamento via `IPagamentoRepositorio`.
    - Incluída lógica para definir status do pagamento (Aprovado ou Recusado) conforme o resultado do gateway.

---
+++ src/Core/FluxoPag.Application/Handlers/CriarComandoDePagamentoHandler.cs
@@ -0,0 +1,51 @@
+﻿using FluxoPag.Application.Commands;
+using FluxoPag.Application.Interfaces;
+using FluxoPag.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluxoPag.Application.Handlers
+{
+    public class CriarComandoDePagamentoHandler : IRequestHandler<CriarComandoDePagamento, Guid>
+    {
+        private readonly IGatewayPagamento _gatewayPagamento;
+        private readonly IPagamentoRepositorio _pagamentoRepositorio;
+        public CriarComandoDePagamentoHandler(IGatewayPagamento gatewayPagamento, IPagamentoRepositorio pagamentoRepositorio)
+        {
+            _gatewayPagamento = gatewayPagamento;
+            _pagamentoRepositorio = pagamentoRepositorio;
+        }
+        public async Task<Guid> Handle(CriarComandoDePagamento request, CancellationToken cancellationToken)
+        {
+            var pagamento = new Pagamento(
+                request.TitularCartao, 
+                request.NumeroCartao, 
+                request.Expira, 
+                request.Cvc, 
+                request.Valor);
+
+            await _pagamentoRepositorio.AddAsync(pagamento);
+            var aprovado = await _gatewayPagamento.ProcessarPagamento(
+                request.TitularCartao, 
+                request.NumeroCartao, 
+                request.Expira, 
+                request.Valor, 
+                request.Cvc);
+
+            if (aprovado)
+            {
+                pagamento.Aprovado();
+            }
+            else
+            {
+                pagamento.Recusado();
+            }
+            await _pagamentoRepositorio.UpdateAsync(pagamento);
+            return pagamento.Id;
+        }
+    }
+}
