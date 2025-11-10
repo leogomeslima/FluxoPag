675f225003a37132eefe55f8183220bc1cd551cd
leogomeslima <leonardo.glima2017@gmail.com>
2025-11-10 00:33:23 -0300

    feat (application): adicionar interface IPagamentorepositorio para abstração de dados de pagamento
    - Criada interface IPagamentorepositorio na camada de aplicação
    - Adicionados métodos assíncronos: AddAsync, GetByIdAsync e UpdateAsync
    - Definida dependência da entidade de domínio Pagamento
    - Estabelecida abstração de banco de dados para futuras implementações de repositório

---
+++ src/Core/FluxoPag.Application/Interfaces/IPagamentoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluxoPag.Domain.Entities;
+
+namespace FluxoPag.Application.Interfaces
+{
+    public interface IPagamentoRepositorio
+    {
+        Task AddAsync(Pagamento pagamentos);
+        Task<Pagamento> GetByIdAsync(Guid id);
+        Task UpdateAsync(Pagamento pagamento);
+    }
+}
