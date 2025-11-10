a976ef886cf72266f8bab8cebda008c9bc207a09
leogomeslima <leonardo.glima2017@gmail.com>
2025-11-10 00:19:10 -0300

    feat(domain): Adicionar entidade Pagamentos com controle de status e lógica de mascaramento

---
+++ src/Core/FluxoPag.Domain/Entities/Pagamentos.cs
@@ -0,0 +1,42 @@
+﻿using FluxoPag.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluxoPag.Domain.Entities
+{
+    public class Pagamentos
+    {
+        public Pagamentos(string titularCartao, string numeroCartao, string expira, string cvc, decimal valor ) 
+        {
+            Id = Guid.NewGuid();
+            TitularCartao = titularCartao;
+            NumeroCartao = "XXXX-XXXX-XXXX-" + numeroCartao.Substring(numeroCartao.Length - 4);
+            Valor = valor;
+            Status = PagamentoStatus.Pendente;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public Guid Id { get; private set; }
+        public string TitularCartao { get; private set; }
+        public string NumeroCartao { get; private set; }
+        public decimal Valor { get; private set; }
+        public PagamentoStatus Status { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime ProcessedAt { get; private set; }
+
+        public void Aprovado()
+        {
+            Status = PagamentoStatus.Aprovado;
+            ProcessedAt = DateTime.UtcNow;
+        }
+
+        public void Recusado()
+        {
+            Status = PagamentoStatus.Recusado;
+            ProcessedAt = DateTime.UtcNow;
+        }
+    }
+}
